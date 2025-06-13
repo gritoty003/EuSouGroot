94427b5bfe35f53cf6e0c8788375b293bdfbc4fb
Guilherme Domingos <e107ti@etec.sp.gov.br>
2025-06-13 19:46:28 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿﻿string Pergunta;
+Console.Clear();
+Console.WriteLine("--- Groot Chatbot ---");
+
+while (true)
+{
+    Console.Write("pergunta: ");
+    Pergunta = Console.ReadLine();
+
+    if (Pergunta == "tchau") 
+    {
+        Console.WriteLine("         Resposta: Eu sou Groot.");
+        return;
+    }
+    else 
+    {
+        Console.WriteLine("         Resposta: Eu sou Groot.");
+        
+    }
+}
